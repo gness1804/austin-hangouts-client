800cd5e5c3aaeafcbbb75e7da280fe669557eea9
gness1804 <graham.nessler@gmail.com>
2018-06-17 19:28:40 -0500

    Places hits api

--- src/views/Places.vue
+++ src/views/Places.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
   .places
+      p(class="places-count") Total places: {{places.length}}
       .places-container(v-if="places.length > 0")
-        p I should not appear.
+        p Hit API worked!
       p(class="no-places-message" v-else) No places yet! Add one now.
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Place, { IPlace } from '../models/Place';
+import axios from 'axios';
+import { IPlace } from '../models/Place';
 
 interface IData {
   places: Array<IPlace>,
+  loadPlacesURL: string,
 }
 
 export default Vue.extend({
@@ -18,7 +21,20 @@ export default Vue.extend({
   data(): IData {
     return {
       places: [],
+      loadPlacesURL: 'http://localhost:7777/places',
     };
+  },
+  methods: {
+    loadPlaces(): void {
+      axios.get(this.loadPlacesURL)
+        .then((res) => {
+          this.places = res.data;
+        })
+        .catch((err) => { throw new Error(`loadPlaces call failed: ${err}`); });
+    },
+  },
+  mounted(): void {
+    this.loadPlaces();
   },
 });
 
