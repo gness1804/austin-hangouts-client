4a5af1e81b8c53254d562b00381fd6a448644f6c
gness1804 <graham.nessler@gmail.com>
2018-06-17 21:46:50 -0500

    Add name and place and photo to container

--- src/views/PlaceContainer.vue
+++ src/views/PlaceContainer.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
   .place-container
+    p {{place.name}}
+    p {{place.address}}
+    img(srce="place.photo" alt="place.name" v-if="place.photo")
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { IPlace } from '../models/Place';
 
+// TODO: change the type to Place
 export default Vue.extend({
   name: 'PlaceContainer',
   props: {
