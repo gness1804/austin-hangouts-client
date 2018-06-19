90065f0c7b914b1c4cb78efd9332a24b7670d43a
gness1804 <graham.nessler@gmail.com>
2018-06-18 21:06:52 -0500

    Add input fields and buttons

--- src/views/AddPlace.vue
+++ src/views/AddPlace.vue
@@ -1,13 +1,32 @@
 <template lang="pug">
     .add-place
-      p I am the Add Place component.
+      h2 Add a Place
+      input(placeholder="Name" v-model.trim="name")
+      input(placeholder="Address" v-model.trim="address")
+      button Add!
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+interface IData {
+  name: string,
+  address: string,
+}
+
 export default Vue.extend({
   name: 'AddPlace',
+  data(): IData {
+    return {
+      name: '',
+      address: '',
+    };
+  },
 });
 </script>
 
+<style>
+  .add-place {
+    text-align: center;
+  }
+</style>
