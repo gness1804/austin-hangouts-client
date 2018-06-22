7e542c28750e0bd80d90025ae81cbdc562c5238e
gness1804 <graham.nessler@gmail.com>
2018-06-21 20:25:01 -0500

    Make successful read api call

--- src/views/EditPlace.vue
+++ src/views/EditPlace.vue
@@ -5,6 +5,8 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import axios from 'axios';
+import config from '../../config';
 
 interface Foo {
   name: string,
@@ -24,6 +26,22 @@ export default Vue.extend({
       Uname: '',
       address: '',
     };
+  },
+  methods: {
+    getPlace(): void {
+      axios.get(`${config.url}/places/${this.placeId}`)
+        .then((res) => {
+          const _place = res.data;
+          this.Uname = res.data.name;
+          this.address = res.data.address;
+        })
+        .catch((err) => {
+          throw new Error(`Error: problem with getPlace: ${err}`);
+        });
+    },
+  },
+  mounted(): void {
+    this.getPlace();
   },
 });
 </script>
